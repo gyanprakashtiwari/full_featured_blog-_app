928f1ca9f115a3b9f43d151693df57b031a911ad
Gyan Prakash Tiwari <golu04822@gmail.com>
2022-02-28 09:19:07 +0530

    trying stuff

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -4,6 +4,8 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY',"60419bc70f2afe01b14dc0acbcc83006")
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
--- run.py
+++ run.py
@@ -6,5 +6,4 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
-    with app.app_context():
-        db.create_all()
+   
